0ac4290164180cff363308485e8242679a0fda1c
ciandre1976 <ciandre23@gmail.com>
2026-02-21 13:46:31 +0100

    bug fixes

--- vite-project/src/main.js
+++ vite-project/src/main.js
@@ -1,31 +1,48 @@
 import * as THREE from "three";
 import { OrbitControls } from "three/addons/controls/OrbitControls.js";
 
-const threejs = new THREE.WebGLRenderer({ antialias: true });
-threejs.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(threejs.domElement);
+class App {
+  #threejs = null;
+  #camera = null;
+  #scene = null;
+  #controls = null;
+  #mesh = null;
 
-const aspect = window.innerWidth / window.innerHeight;
-const camera = new THREE.PerspectiveCamera(50, aspect, 0.1, 2000);
+  constructor() {}
+  Initialize() {
+    this.#threejs = new THREE.WebGLRenderer({ antialias: true });
+    this.#threejs.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(this.#threejs.domElement);
 
-camera.position.z = 5;
+    const aspect = window.innerWidth / window.innerHeight;
+    const camera = new THREE.PerspectiveCamera(50, aspect, 0.1, 2000);
+    camera.position.z = 5;
+    this.#camera = camera;
 
-const controls = new OrbitControls(camera, threejs.domElement);
-controls.enableDamping = true;
-controls.target.set(0, 0, 0);
+    this.#controls = new OrbitControls(this.#camera, this.#threejs.domElement);
+    this.#controls.enableDamping = true;
+    this.#controls.target.set(0, 0, 0);
 
-const scene = new THREE.Scene();
+    this.#scene = new THREE.Scene();
 
-const mesh = new THREE.Mesh(
-  new THREE.BoxGeometry(),
-  new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }),
-);
-scene.add(mesh);
+    this.#mesh = new THREE.Mesh(
+      new THREE.BoxGeometry(),
+      new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true }),
+    );
+    this.#scene.add(this.#mesh);
+  }
 
-function render() {
-  controls.update();
-  mesh.rotation.y += 0.01;
-  threejs.render(scene, camera);
-  requestAnimationFrame(render);
+  Run() {
+    const render = () => {
+      this.#controls.update();
+      this.#mesh.rotation.y += 0.01;
+      this.#threejs.render(this.#scene, this.#camera);
+      requestAnimationFrame(render);
+    };
+    render();
+  }
 }
-render();
+
+const Appplication = new App();
+Appplication.Initialize();
+Appplication.Run();
